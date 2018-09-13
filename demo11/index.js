f86958fbd74d50a8cf4355096fb4faf4a720c4f2
liuyajun <liuyajun@linhuiba.com>
2018-09-13 15:31:38 +0800

    如何创建 Node.js 模块

--- demo11/index.js
+++ demo11/index.js
@@ -1,5 +1,7 @@
 // document.write('<h1>Hello World</h1>');
 
 exports.printMsg = function() {
-	console.log('This is a message from the demo package, and test version');
+	console.log('This is a message from the demo package, and test version 1.0.2');
 }
+
+this.printMsg();
